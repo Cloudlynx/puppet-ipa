9503fdf541c1177d1b824abbfafee7ed618eac3b
Michael Gruener <michael.gruener@cloudlynx.ch>
2015-01-28 13:47:31 +0100

    Fix typo in sftp parameters

--- manifests/replicaprepare.pp
+++ manifests/replicaprepare.pp
@@ -20,7 +20,7 @@ define ipa::replicaprepare (
   }
 
   exec { "replica-info-copy-${host}":
-    command     => "/bin/echo put ${file} data/|/usr/bin/sftp -o IdentityFile=${sshkeyfile} -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o ConnectTimeout=5 -o ServerAliveInterval=2 -p - ipareplica@${host}",
+    command     => "/bin/echo put ${file} data/|/usr/bin/sftp -o IdentityFile=${sshkeyfile} -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o ConnectTimeout=5 -o ServerAliveInterval=2 -b - ipareplica@${host}",
     refreshonly => true,
     tries       => '60',
     try_sleep   => '60'
